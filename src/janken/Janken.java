41272b9923907b1302502417196059ba02f7fac1
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2019-08-24 19:53:00 +0900

    update 変数名の変更

--- src/janken/Janken.java
+++ src/janken/Janken.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 
 public class Janken {
 	public static void  main(String[] args) {
-		int m = 0;
-		int e = 0;
+		int win_count = 0;
+		int lose_count = 0;
 		while(true) {
-			System.out.println( m + e);
 			System.out.println("手を入力してください");
 			System.out.println("1:グー, 2:パー, 3: チョキ");
 
@@ -15,25 +14,25 @@ public class Janken {
 //			相手の手決定
 			int enemy = new java.util.Random().nextInt(4);
 //			手の判定
-			System.out.println("あなたの手は" + judge(me) +"です");
-			System.out.println("てきの手は" + judge(enemy) +"です");
+			System.out.println("あなたの手は" + judge_hand(me) +"です");
+			System.out.println("てきの手は" + judge_hand(enemy) +"です");
 //			勝敗判定
 			if(me == enemy) {
 				System.out.println("あいこです");
 			}else if((me == 1 && enemy == 3) || (me == 2 && enemy == 1) || (me == 3 && enemy == 2)){
 				System.out.println("あなたの勝ちだ！  (*ﾟ▽ﾟ)/ﾟ･:*【祝】*:･ﾟ＼(ﾟ▽ﾟ*)");
-				m++;
+				win_count++;
 			}else {
 				System.out.println("お前の負けだ　( `д´)、ﾍﾟｯﾍﾟｯ");
-				e++;
+				lose_count++;
 			}
-			if(m + e == 5) {
+			if(win_count + lose_count == 5) {
 				break;
 			}
 		}
 
-		System.out.println("あなたの戦績 : " + m + "勝," +  e+ "負");
-		if (m > e) {
+		System.out.println("あなたの戦績 : " + win_count + "勝," +  lose_count + "負");
+		if (win_count > lose_count) {
 			System.out.println("あなたの勝ちだーーー！！！");
 			System.out.println("ヽ(´□｀。)ﾉ・ﾟﾔｯﾀｱｧｧｱｧﾝ.");
 		}else {
@@ -42,7 +41,7 @@ public class Janken {
 		}
 	}
 
-	public static String judge(int te) {
+	public static String judge_hand(int te) {
 		String my_te = "";
 		if(te == 1) {
 			my_te = "グー";
